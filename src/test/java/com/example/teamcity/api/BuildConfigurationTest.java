fdf9884a6adf95ae8c073e5f83725c1227b1cc84
Irina Levchenko <irina.levchenko@semrush.com>
2024-03-01 02:15:48 +0100

    BuildConfigurationTest update

--- src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -1,7 +1,6 @@
 package com.example.teamcity.api;
 
-import com.example.teamcity.api.requests.checked.CheckedProject;
-import com.example.teamcity.api.requests.checked.CheckedUser;
+import com.example.teamcity.api.requests.CheckedRequests;
 import com.example.teamcity.api.spec.Specifications;
 import org.testng.annotations.Test;
 
@@ -9,14 +8,34 @@ public class BuildConfigurationTest extends BaseApiTest {
     @Test
     public void buildConfigurationTest() {
         var testData = testDataStorage.addTestData();
+        CheckedRequests checkedRequests;
 
-        new CheckedUser(Specifications.getSpec().superUserSpec())
+        checkedWithSuperUser.getUserRequest()
                 .create(testData.getUser());
 
-        var project = new CheckedProject(Specifications.getSpec()
-                .authSpec(testData.getUser()))
+        checkedRequests = new CheckedRequests(Specifications.getSpec()
+                .authSpec(testData.getUser()));
+
+        checkedRequests
+                .getProjectRequest()
                 .create(testData.getProject());
 
-        softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
+        var buildConfig = checkedRequests
+                .getBuildConfigRequest()
+                .create(testData.getBuildType());
+
+        softy.assertThat(buildConfig.getId())
+                .isEqualTo(testData.getBuildType()
+                        .getId());
     }
+
+//        new CheckedUser(Specifications.getSpec().superUserSpec())
+//                .create(testData.getUser());
+//
+//        var project = new CheckedProject(Specifications.getSpec()
+//                .authSpec(testData.getUser()))
+//                .create(testData.getProject());
+//
+//        softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
+//    }
 }
